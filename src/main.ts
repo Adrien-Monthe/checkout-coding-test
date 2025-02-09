d96f46ee341190ca1d287d35a1d32b24a8883f76
Adrien Monthe <montheadrien@gmail.com>
2025-02-10 00:58:27 +0100

    feat: add swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Session Booking API')
+    .setDescription('The Session Booking API description')
+    .setVersion('1.0')
+    .addTag('Sessions')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
